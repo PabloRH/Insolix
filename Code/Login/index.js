991843b899d33a4ad47f977e16abe01855f2cd9b
Pablo RH <prosash1600.alumno.ipn.mx>
2019-05-07 22:58:38 -0500

    Add better support

--- Code/Login/index.js
+++ Code/Login/index.js
@@ -1,91 +1,112 @@
-import React, { Fragment } from 'react';
+import React, { Fragment } from "react";
 import { withRouter } from "react-router-native";
-import { Button, TextInput, ActivityIndicator, Colors } from 'react-native-paper';
-import { View, Alert } from 'react-native'
+import {
+  Button,
+  TextInput,
+  ActivityIndicator,
+  Colors
+} from "react-native-paper";
+import { View, Alert } from "react-native";
 
-import MyHeader from "../Header"
-import MyStyle from "../styles"
-import { Icon } from 'native-base';
-//import { Data } from "../App/Data" 
+import MyHeader from "../Header";
+import MyStyle from "../styles";
+import { Icon } from "native-base";
+import { Data } from "../App/Data";
 
 class Login extends React.Component {
-  state = {user: "", password: "", loading: false}
-
-  sendToDB = () => {
-    if (this.state.loading) return
-    const data = {user: this.state.user, password: this.state.password};
+  state = { user: "", password: "", loading: false };
+  static contextType = Data;
+  sendToDB = setter => {
+    if (this.state.loading) return;
+    const data = { user: this.state.user, password: this.state.password };
     const options = {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(data)
     };
 
-    fetch('http://pablorosas.pythonanywhere.com/logIn', options)
+    fetch("http://pablorosas.pythonanywhere.com/logIn", options)
       .then(res => res.json())
       .then(res => {
         if (res.ID == null) {
-          Alert.alert('¡Oh ha ocurrido un error!',
-          'No existe este Usuario\nΣ(▼ □ ▼メ)',
-          [{text: '¿Quieres Regitarte?', onPress: () => this.props.history.push("/SignUp")}])
-          this.setState({loading: false})
+          Alert.alert(
+            "¡Oh ha ocurrido un error!",
+            "No existe este Usuario\nΣ(▼ □ ▼メ)",
+            [
+              {
+                text: "¿Quieres Regitarte?",
+                onPress: () => this.props.history.push("/SignUp")
+              }
+            ]
+          );
+          this.setState({ loading: false });
+        } else {
+          this.props.history.push("/SignedIn");
+          setter(res);
+        }
+      });
 
-        } else{
-          console.log(res)
-          this.props.history.push("/SignedIn")
-        }  
-        })
-    
-    this.setState({loading: true})
-  }
+    this.setState({ loading: true });
+  };
 
-  render () {
-
+  render() {
     const textProps = {
-      mode: 'outlined',
+      mode: "outlined",
       style: MyStyle.input,
-      theme: { colors:{text:'black'}}
-    }
+      theme: { colors: { text: "black" } }
+    };
 
     return (
       <Fragment>
         <MyHeader text="Derbild" subtitle="Log In" link="/" hasAnArrow />
-        <View style={MyStyle.appContainer}> 
+        <View style={MyStyle.appContainer}>
           <View style={MyStyle.sideIcon}>
             <Icon name="person" />
-            <TextInput 
-              {...textProps} 
-              label = 'User'
-              value = {this.state.user} 
-              onChange = {e => this.setState({user: e.nativeEvent.text})}
+            <TextInput
+              {...textProps}
+              label="User"
+              value={this.state.user}
+              onChange={e => this.setState({ user: e.nativeEvent.text })}
             />
           </View>
           <View style={MyStyle.sideIcon}>
             <Icon name="lock" />
-            <TextInput 
-              {...textProps} 
-              label = 'Password'
+            <TextInput
+              {...textProps}
+              label="Password"
               secureTextEntry
-              password 
-              value = {this.state.password} 
-              onChange = {e => this.setState({password: e.nativeEvent.text})}
+              password
+              value={this.state.password}
+              onChange={e => this.setState({ password: e.nativeEvent.text })}
             />
           </View>
-          <Button icon="send" mode="outlined" onPress={this.sendToDB} style={MyStyle.btn}>
-            Log In
-          </Button>
-
-          {
-            this.state.loading && 
-            <ActivityIndicator animating={true} size={"large"} color={Colors.red800} />
-          }
-
+          <Data.Consumer>
+            {context => {
+              const { setter } = context;
+              return (
+                <Button
+                  icon="send"
+                  mode="outlined"
+                  onPress={() => this.sendToDB(setter)}
+                  style={MyStyle.btn}
+                >
+                  Log In
+                </Button>
+              );
+            }}
+          </Data.Consumer>
+          {this.state.loading && (
+            <ActivityIndicator
+              animating={true}
+              size={"large"}
+              color={Colors.red800}
+            />
+          )}
         </View>
       </Fragment>
     );
-  }  
-};
-
-
-// Login.contextType = Data;
+  }
+}
 
 export default withRouter(Login);
+
