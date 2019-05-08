d35e1beac9b9d159c04a004415f4ef9173a9da71
Pablo RH <prosash1600.alumno.ipn.mx>
2019-05-08 18:10:54 -0500

    Update

--- Code/SignedIn/Gallery/index.js
+++ Code/SignedIn/Gallery/index.js
@@ -10,75 +10,85 @@ import { HasToUpdate } from "../state";
 
 class Gallery extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     const Photos = Array.from({ length: 4 }).map(
-      (_, i) => `https://unsplash.it/300/300/?random&__id=${this.props.route.key}${i}`
-    )
-    this.state = { Photos, refreshing: false }
+      (_, i) =>
+        `https://unsplash.it/300/300/?random&__id=${this.props.route.key}${i}`
+    );
+    this.state = { Photos, refreshing: false };
   }
-  
-  getPhotos = (id) => {
+
+  getPhotos = id => {
     const options = {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ id })
     };
-    console.log("Gettign photos " + id)
+    console.log("Gettign photos " + id);
     fetch("http://pablorosas.pythonanywhere.com/GetPhotos", options)
       .then(res => res.json())
-      .then(res => {
-        if (res == null) return 
-        const newPhotos = res.filter( (photo, index, self) => self.indexOf(photo) === index)
-        const myPhotos = newPhotos.map(photo => `http://pablorosas.pythonanywhere.com/static/${photo.HashID}`)
-        console.log(myPhotos)
-        this.setState({Photos: [...myPhotos, ...this.state.Photos], refreshing: false})
+      .then(newPhotos => {
+        if (newPhotos == null) return;
+        const myPhotos = newPhotos.map(
+          photo => `http://pablorosas.pythonanywhere.com/static/${photo.HashID}`
+        );
+        let newPhotosUnique = [...myPhotos, ...this.state.Photos];
+        newPhotosUnique = newPhotosUnique.filter(
+          (photo, index, self) => self.indexOf(photo) === index
+        );
+        console.log(newPhotosUnique);
+        this.setState({ Photos: newPhotosUnique, refreshing: false });
       });
-  }
+  };
 
   render() {
-    console.log(this.state.Photos)
+    console.log(this.state.Photos);
     return (
       <HasToUpdate.Consumer>
-        { uploading => (
-        <Data.Consumer>
-          {context => {
-            console.log(uploading)
-            if (uploading.getState()) {
-              this.getPhotos(context.state.ID)
-              uploading.setToFalse()
-            }
-            const { state } = context;
-            return (
-              <Fragment>
-                <MyHeader
-                  text="Gallery"
-                  subtitle={state.Type}
-                  link="/"
-                  hasSetting
-                />
-                <View style={{marginBottom: 85}}>
-                  <ScrollView contentContainerStyle={MyStyles.content} refreshControl={
-                    <RefreshControl refreshing={this.state.refreshing} onRefresh = {() => {
-                      if (uploading.getState()) {
-                        this.setState({refreshing: true})
-                        this.getPhotos(context.state.ID)
-                        uploading.setToFalse()
+        {uploading => (
+          <Data.Consumer>
+            {context => {
+              console.log(uploading);
+              if (uploading.getState()) {
+                this.getPhotos(context.state.ID);
+                uploading.setToFalse();
+              }
+              const { state } = context;
+              return (
+                <Fragment>
+                  <MyHeader
+                    text="Gallery"
+                    subtitle={state.Type}
+                    link="/"
+                    hasSetting
+                  />
+                  <View style={{ marginBottom: 85 }}>
+                    <ScrollView
+                      contentContainerStyle={MyStyles.content}
+                      refreshControl={
+                        <RefreshControl
+                          refreshing={this.state.refreshing}
+                          onRefresh={() => {
+                            if (uploading.getState()) {
+                              this.setState({ refreshing: true });
+                              this.getPhotos(context.state.ID);
+                              uploading.setToFalse();
+                            }
+                          }}
+                        />
                       }
-                    }} />
-                  }>
-                    {
-                      this.state.Photos.map(uri => (
-                      <View key={uri} style={MyStyles.item}>
-                        <Image source={{ uri }} style={MyStyles.photo} />
-                      </View>
-                    ))
-                    }
-                  </ScrollView>
-                </View>
-              </Fragment>
-            );
-          }}
-        </Data.Consumer>
+                    >
+                      {this.state.Photos.map(uri => (
+                        <View key={uri} style={MyStyles.item}>
+                          <Image source={{ uri }} style={MyStyles.photo} />
+                        </View>
+                      ))}
+                    </ScrollView>
+                  </View>
+                </Fragment>
+              );
+            }}
+          </Data.Consumer>
         )}
       </HasToUpdate.Consumer>
     );
